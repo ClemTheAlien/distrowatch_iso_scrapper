84b4c4d0e3e1476ba33b3e01d402a0b15aaa1af3
ClemTheAlien <ClemTheAlien@proton.me>
2025-07-26 09:28:42 -0500

    initialized

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+
+pkgs.mkShellNoCC {
+
+  packages = with pkgs; [
+    python3
+  ];
+  shellHook = ''
+  source ./.venv/bin/activate
+  pip install requirments.txt
+  '';
+
+}
