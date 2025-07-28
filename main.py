a242e5c55acf3535538ad330f52d49ed6870b4be
ClemTheAlien <ClemTheAlien@proton.me>
2025-07-28 08:39:35 -0500

    chore(main.py):Linted the code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -22,29 +20,8 @@ def create_firefox_driver():
         return None
 
 
-def search(distro):
-    try:
-        if distro is not None:
-            print("Getting link for " + distro)
-            try:
-                input_element = driver.find_element(
-                    By.CSS_SELECTOR, "input[value='Type Distribution Name']"
-                )
-                input_element.send_keys(distro, Keys.ENTER)
-                try:
-                    results = find_links()
-                    parse(results)
-                except Exception as e:
-                    print(f"An error occurred during script execution1: {e}")
-            except Exception as e:
-                print(f"An error occurred during script execution2: {e}")
-    except Exception as e:
-        print(f"An error occurred during script execution3: {e}")
-
-
 def find_links():  # TODO: make it stop scrapping only the front page by using current_link
     html_content = None
-    keywords = ["iso", "ISO"]
     found_links = []
     # current_link = driver.current_url
 
@@ -52,10 +29,10 @@ def find_links():  # TODO: make it stop scrapping only the front page by using c
         html_content = driver.page_source
         print("Successfully fetched HTML content.")
         soup = BeautifulSoup(html_content, "html.parser")
-        all_links = soup.find_all("a",string="ISO")
+        all_links = soup.find_all("a", string="ISO")
         for link in all_links:
             href = link.get("href")
-            found_links.append(link)
+            found_links.append(href)
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
     return found_links
@@ -71,7 +48,7 @@ if __name__ == "__main__":
     try:
         driver = create_firefox_driver()
         if driver:
-            driver.get("https://distrowatch.com/"+distro)
+            driver.get("https://distrowatch.com/" + distro)
             results = find_links()
             parse(results)
     except Exception as e:
